b0ff0e49630a9a00cd9d117752927844a8d98a16
desktopwork <jtsupro@gmail.com>
2014-05-08 18:29:46 -0700

    add powercli module

---
+++ vSphere-Utils.psm1
@@ -0,0 +1,43 @@
+Function Connect-vCenter {
+  param(
+    [String]$server="winvcenter5.irvineqa.local",
+    [String]$user="administrator@vsphere.local",
+    [String]$password="P3t3rPan@1"  
+    )
+  Connect-ViServer -server $server -user $user -password $password
+}
+
+Function Disconnect-vCenter {
+  Disconnect-ViServer -server * -confirm:$False
+}
+
+
+Function Create-VM {
+    <#
+    .SYNOPSIS
+      Create VM
+    .DESCRIPTION
+      The function will create VM
+    .NOTES
+      Source: Automating vSphere Administration
+      Authors: Avamar VMware QA
+      Date: 5/8/2014
+    .PARAMETER VM
+    .PARAMETER NUM
+    .Example
+      PS> Create-VM
+    #>
+
+  param(
+     [String]$VM="vmtest", 
+     [String]$ESXHOST="esxvcb3.asl.lab.emc.com",
+     [String]$DATASTORE="iLUN0",
+     [Int]$NUM
+     [Int]$STARTNO=1
+  )    
+  $NUM = $NUM + 1
+  For ($i = $STARTNO; $i -lt $NUM;$i++) {
+    New-VM -Name $VM$i -ResourcePool $ESXHOST -Datastore $DATASTORE -NumCPU 2 -MemoryGB 2 -DiskGB 2 -NetworkName "VLAN244" `
+      -Floppy -CD -DiskStorageFormat Thin -GuestID winNetDatacenterGuest
+  } 
+}
